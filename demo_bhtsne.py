86973732e7db1b817224e8fffb2bc82da2ce332a
StoneKahuna <stone@wizr.com>
2018-04-03 13:03:29 -0700

    rearrange file structure

---
+++ demo_bhtsne.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pylab
+import sys
+from datetime import datetime
+
+sys.path.append('./wrapper')
+import bhtsne
+
+print('Loading data...')
+data = np.loadtxt("./data/mnist2500_X.txt")
+labels = np.loadtxt("./data/mnist2500_labels.txt")
+figfile = './results/bhtsne_demo.png'
+
+print('Processing...')
+start = datetime.now()
+embedding_array = bhtsne.run_bh_tsne(data, initial_dims=data.shape[1])
+end = datetime.now()
+elapsedTime = (end - start).total_seconds()
+print('Process time ', elapsedTime, 's')
+
+
+pylab.scatter(embedding_array[:, 0], embedding_array[:, 1], 20, labels)
+pylab.savefig(figfile)
+pylab.show()
